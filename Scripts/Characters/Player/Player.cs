427fe21b1c8d325e4b14f1715041024230f7a0fc
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-14 11:47:41 -0600

    Overriding Methods

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        //GD.Print("player physics process");
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        GD.Print(@event);
+    }
 }
