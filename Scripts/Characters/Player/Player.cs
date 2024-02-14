749de19edf4a38435b8d466caa841488d0f520e2
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-14 12:25:07 -0600

    Flipping the Player

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -19,6 +19,7 @@ public partial class Player : CharacterBody3D
         Velocity *= 5;
 
         MoveAndSlide();
+        Flip();
     }
 
     public override void _Input(InputEvent @event)
@@ -36,5 +37,15 @@ public partial class Player : CharacterBody3D
         {
             animationPlayerNode.Play(GameConstants.ANIM_MOVE);
         }
+    }
+
+    private void Flip()
+    {
+        bool isNotMovingHorizontally = Velocity.X == 0;
+
+        if (isNotMovingHorizontally) { return;}
+
+        bool isMovingLeft = Velocity.X < 0;
+        sprite3DNode.FlipH = isMovingLeft;
     }
 }
