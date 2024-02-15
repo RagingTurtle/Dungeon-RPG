4b99efc370f09e8102cd3fb6c2e49d60ac753f33
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-15 09:24:21 -0600

    Creating an Idle and Move State

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -4,14 +4,9 @@ using System;
 public partial class Player : CharacterBody3D
 {
     [ExportGroup("Required Nodes")]
-    [Export] private AnimationPlayer animationPlayerNode;
-    [Export] private Sprite3D sprite3DNode;
+    [Export] public AnimationPlayer animationPlayerNode;
+    [Export] public Sprite3D sprite3DNode;
     private Vector2 direction = new();
-
-    public override void _Ready()
-    {
-        animationPlayerNode.Play(GameConstants.ANIM_IDLE);
-    }
 
     public override void _PhysicsProcess(double delta)
     {
@@ -28,15 +23,6 @@ public partial class Player : CharacterBody3D
             GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT,
             GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_BACKWARD
         );
-
-        if(direction == Vector2.Zero)
-        {
-            animationPlayerNode.Play(GameConstants.ANIM_IDLE);
-        }
-        else
-        {
-            animationPlayerNode.Play(GameConstants.ANIM_MOVE);
-        }
     }
 
     private void Flip()
