5484298d566a8dd36146760cbe19e44c39ab89ab
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-18 21:52:27 -0600

    Chasing the Player

--- Scripts/Characters/Enemy/EnemyChaseState.cs
+++ Scripts/Characters/Enemy/EnemyChaseState.cs
@@ -1,11 +1,32 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class EnemyChaseState : EnemyState
 {
+    [Export] private Timer chaseTimerNode;
+    private CharacterBody3D target;
     protected override void EnterState()
     {
         characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_MOVE);
-        GD.Print("chase state");
+        target = characterNode.ChaseAreaNode.GetOverlappingBodies().First() as CharacterBody3D;
+
+        chaseTimerNode.Timeout += HandleTimeout;
+    }
+
+    protected override void ExitState()
+    {
+        chaseTimerNode.Timeout -= HandleTimeout;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Move();
+    }
+
+    private void HandleTimeout()
+    {
+        destination = target.GlobalPosition;
+        characterNode.AgentNode.TargetPosition = destination;   
     }
 }
