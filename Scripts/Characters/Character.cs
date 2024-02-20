dbfe961de33b6ae95f6ae2067d05ac062ae0fc8f
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-19 20:14:36 -0600

    Hitboxes and Hurtboxes

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -7,6 +7,7 @@ public abstract partial class Character : CharacterBody3D
     [Export] public AnimationPlayer AnimationPlayerNode {get; private set;}
     [Export] public Sprite3D Sprite3DNode {get; private set;}
     [Export] public StateMachine StateMachineNode {get; private set;}
+    [Export] public Area3D HurtboxNode {get; private set;}
 
     [ExportGroup("AI Nodes")]
     [Export] public Path3D PathNode {get; private set;}
@@ -16,6 +17,16 @@ public abstract partial class Character : CharacterBody3D
     [Export] public Area3D AttackAreaNode {get; private set;}
 
     public Vector2 direction = new();
+
+    public override void _Ready()
+    {
+        HurtboxNode.AreaEntered += HandleHurtboxEntered;
+    }
+
+    private void HandleHurtboxEntered(Area3D area)
+    {
+        GD.Print(area.Name);
+    }
 
     public void Flip()
     {
