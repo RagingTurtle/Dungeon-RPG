6dee6ca63decc9fd5b2339e69fc24ed419a615b3
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-19 19:40:38 -0600

    Combo Counters

--- Scripts/Characters/Player/PlayerAttackState.cs
+++ Scripts/Characters/Player/PlayerAttackState.cs
@@ -3,4 +3,26 @@ using System;
 
 public partial class PlayerAttackState : PlayerState
 {
+    private int comboCounter = 1;
+    private int maxComboCount = 2;
+
+    protected override void EnterState()
+    {
+        characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter);
+
+        characterNode.AnimationPlayerNode.AnimationFinished += HandleAnimationFinished;
+    }
+
+    protected override void ExitState()
+    {
+        characterNode.AnimationPlayerNode.AnimationFinished -= HandleAnimationFinished;
+    }
+
+    private void HandleAnimationFinished(StringName animName)
+    {
+        comboCounter = Mathf.Wrap(comboCounter + 1, 1, maxComboCount + 1);
+
+        characterNode.StateMachineNode.SwitchState<PlayerIdleState>();
+    }
+
 }
