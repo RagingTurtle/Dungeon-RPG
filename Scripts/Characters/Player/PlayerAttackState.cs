21ed0772aa9df66bda6b8fa31e537bb8654d0f4b
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-19 19:48:57 -0600

    Resetting the Combo Counter With a Timer

--- Scripts/Characters/Player/PlayerAttackState.cs
+++ Scripts/Characters/Player/PlayerAttackState.cs
@@ -3,12 +3,20 @@ using System;
 
 public partial class PlayerAttackState : PlayerState
 {
+    [Export] private Timer comboTimerNode;
     private int comboCounter = 1;
     private int maxComboCount = 2;
 
+    public override void _Ready()
+    {
+        base._Ready();
+        
+        comboTimerNode.Timeout += () => comboCounter = 1;
+    }
+
     protected override void EnterState()
     {
-        characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter);
+        characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_ATTACK + comboCounter, -1, 1.5f);
 
         characterNode.AnimationPlayerNode.AnimationFinished += HandleAnimationFinished;
     }
@@ -16,6 +24,8 @@ public partial class PlayerAttackState : PlayerState
     protected override void ExitState()
     {
         characterNode.AnimationPlayerNode.AnimationFinished -= HandleAnimationFinished;
+
+        comboTimerNode.Start();
     }
 
     private void HandleAnimationFinished(StringName animName)
