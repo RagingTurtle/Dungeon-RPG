1fea9f0cf6d3c84e3c4ced211a663622c9d094c7
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-23 09:53:56 -0600

    Moving the Enemy Hitbox

--- Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -1,10 +1,23 @@
 using Godot;
 using System;
+using System.Linq;
+using System.Threading.Channels;
 
 public partial class EnemyAttackState : EnemyState
 {
+    private Vector3 targetPosition;
     protected override void EnterState()
     {
         characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_ATTACK);
+
+        Node3D target = characterNode.AttackAreaNode.GetOverlappingBodies().First();
+
+        targetPosition = target.GlobalPosition;
+    }
+
+    private void PerformHit()
+    {
+        characterNode.ToggleHitbox(false);
+        characterNode.HitboxNode.GlobalPosition = targetPosition;
     }
 }
