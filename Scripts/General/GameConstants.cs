0481cdbfeaced8a2ca18e3d082353e5f4241d03a
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-23 10:38:21 -0600

    Character Death States

--- Scripts/General/GameConstants.cs
+++ Scripts/General/GameConstants.cs
@@ -5,6 +5,7 @@ public class GameConstants
     public const string ANIM_MOVE = "Move";
     public const string ANIM_DASH = "Dash";
     public const string ANIM_ATTACK = "Attack";
+    public const string ANIM_DEATH ="Death";
 
     public const string INPUT_MOVE_LEFT = "MoveLeft";
     public const string INPUT_MOVE_RIGHT = "MoveRight";
