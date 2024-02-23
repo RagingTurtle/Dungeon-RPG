831b9400f513642b3c0647c20d52a85148d934e8
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-23 10:12:54 -0600

    Finishing the Enemy Attack State

--- Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -13,6 +13,41 @@ public partial class EnemyAttackState : EnemyState
         Node3D target = characterNode.AttackAreaNode.GetOverlappingBodies().First();
 
         targetPosition = target.GlobalPosition;
+
+        characterNode.AnimationPlayerNode.AnimationFinished += HandleAnimationFinished;
+    }
+
+    protected override void ExitState()
+    {
+        characterNode.AnimationPlayerNode.AnimationFinished -= HandleAnimationFinished;
+
+    }
+
+    private void HandleAnimationFinished(StringName animName)
+    {
+        characterNode.ToggleHitbox(true);
+
+        Node3D target = characterNode.AttackAreaNode.GetOverlappingBodies().FirstOrDefault();
+
+        if (target == null)
+        {
+            Node3D chaseTarget = characterNode.ChaseAreaNode.GetOverlappingBodies().FirstOrDefault();
+
+            if (chaseTarget == null)
+            {
+                characterNode.StateMachineNode.SwitchState<EnemyReturnState>();
+                return;
+            }
+
+            characterNode.StateMachineNode.SwitchState<EnemyChaseState>();
+            return;
+        }
+
+        characterNode.AnimationPlayerNode.Play(GameConstants.ANIM_ATTACK);
+        targetPosition = target.GlobalPosition;
+
+        Vector3 direction =  characterNode.GlobalPosition.DirectionTo(targetPosition);
+        characterNode.Sprite3DNode.FlipH = direction.X < 0;
     }
 
     private void PerformHit()
