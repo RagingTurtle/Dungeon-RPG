d9acdc0ccfd0b29d5c7ba879fc62f9a50a5e3058
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-25 19:05:41 -0600

    Grabbing UI Containers

---
+++ Scripts/UI/UIController.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Net.Mime;
+
+public partial class UIController : Control
+{
+    private Dictionary<ContainerType, UIContainer> containers;
+    public override void _Ready()
+    {
+        containers = GetChildren().Where((element) => element is UIContainer).Cast<UIContainer>().ToDictionary((element) => element.container);
+
+        containers[ContainerType.Start].Visible = true;
+    }
+}
