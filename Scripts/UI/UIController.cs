3423e6fd116d8cfd360cca1948769b0bec2724e7
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-25 21:24:58 -0600

    Pause UI

--- Scripts/UI/UIController.cs
+++ Scripts/UI/UIController.cs
@@ -8,18 +8,42 @@ using System.Net.Mime;
 public partial class UIController : Control
 {
     private Dictionary<ContainerType, UIContainer> containers;
+    private bool canPause = false;
     public override void _Ready()
     {
         containers = GetChildren().Where((element) => element is UIContainer).Cast<UIContainer>().ToDictionary((element) => element.container);
 
         containers[ContainerType.Start].Visible = true;
         containers[ContainerType.Start].ButtonNode.Pressed += HandleStartPressed;
+        containers[ContainerType.Pause].ButtonNode.Pressed += HandlePausePressed;
         GameEvents.OnEndGame += HandleEndGame;         
         GameEvents.OnVictory += HandleVicotry;
     }
 
+    private void HandlePausePressed()
+    {
+        GetTree().Paused = false;
+        containers[ContainerType.Pause].Visible = false;
+        containers[ContainerType.Stats].Visible = true;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (!canPause) { return; }
+        
+        if(!Input.IsActionJustPressed(GameConstants.INPUT_PAUSE))
+        {
+            return;
+        }
+
+        containers[ContainerType.Stats].Visible = GetTree().Paused;
+        GetTree().Paused = !GetTree().Paused;
+        containers[ContainerType.Pause].Visible = GetTree().Paused;
+    }
     private void HandleVicotry()
     {
+        canPause = false;
+
         containers[ContainerType.Stats].Visible = false;
         containers[ContainerType.Victory].Visible = true;
 
@@ -28,12 +52,16 @@ public partial class UIController : Control
 
     private void HandleEndGame()
     {
+        canPause = false;
+
         containers[ContainerType.Stats].Visible = false;
         containers[ContainerType.Defeat].Visible = true;
     }
 
     private void HandleStartPressed()
     {
+        canPause = true;
+
         GetTree().Paused = false;
 
         containers[ContainerType.Start].Visible = false;
