ea14fec7a70bd15570fb8ea0bea61645a898ae40
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2024-02-28 10:20:19 -0600

    Dynamically Applying a Shader

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -14,19 +14,34 @@ public abstract partial class Character : CharacterBody3D
     [Export] public Area3D HurtboxNode {get; private set;}
     [Export] public Area3D HitboxNode {get; private set;}
     [Export] public CollisionShape3D HitboxShapeNode {get; private set;}
+    [Export] public Timer ShaderTimerNode {get; private set;}
 
     [ExportGroup("AI Nodes")]
     [Export] public Path3D PathNode {get; private set;}
     [Export] public NavigationAgent3D AgentNode {get; private set;}
     [Export] public Area3D ChaseAreaNode {get; private set;}
-
     [Export] public Area3D AttackAreaNode {get; private set;}
 
     public Vector2 direction = new();
+    private ShaderMaterial shader;
 
     public override void _Ready()
     {
+        shader = (ShaderMaterial)Sprite3DNode.MaterialOverlay;
+
         HurtboxNode.AreaEntered += HandleHurtboxEntered;
+        Sprite3DNode.TextureChanged += HandleTextureChanged;
+        ShaderTimerNode.Timeout += HandleShaderTimeout;
+    }
+
+    private void HandleShaderTimeout()
+    {
+        shader.SetShaderParameter("active", false);
+    }
+
+    private void HandleTextureChanged()
+    {
+        shader.SetShaderParameter("tex", Sprite3DNode.Texture);
     }
 
     private void HandleHurtboxEntered(Area3D area)
@@ -40,6 +55,10 @@ public abstract partial class Character : CharacterBody3D
         health.StatValue -= damage;
 
         GD.Print(health.StatValue);
+
+        shader.SetShaderParameter("active", true);
+
+        ShaderTimerNode.Start();
     }
 
     public StatResource GetStatResource(Stat stat)
